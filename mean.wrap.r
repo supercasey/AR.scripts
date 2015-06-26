fffa13479cbd8ece99f7b64fbf7921accb5427a5
Carl Schroedl <cschroedl@usgs.gov>
2015-06-26 16:21:53 -0500

    Updating water year in comment

--- mean.wrap.r
+++ mean.wrap.r
@@ -1,7 +1,7 @@
 			#' Writes shapefiles computed by annualmay.meanlinemaps.r 
 			#' This script depends on the presence of a user-input shapefile, "missriv" of mississippi river streams, "a.ll", a file of all annual loads in the basin, and "m.ll" a file of all monthly loads in the basin
 			#' @param consts is a character vector representing  the constituents to be mapped, currently defined as NO3_NO2, TN, and TP
-			#' @param wycur is a  number representing the most recent water year published on the annual reporting wiste are computed, currently 2013
+			#' @param wycur is a  number representing the most recent water year published on the annual reporting wiste are computed, currently 2014
 			#' @param ptypes are a character vector of types of loads, currently they include "WY" or "May"
 			#' @param periods represent the periods in which mean loads are to be computed over; this is indicated by the starting year in which loads are to be computed, currently either 1980 or 1993 
 			#' This script writes shape files with WY and May loads for each segment for May and Annual loads, shapefiles are named by constituent and type of mean value
